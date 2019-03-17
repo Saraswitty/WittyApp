ca11625fa1d757e977a641e338094caf28d64a1d
Ajay Nair <ajaynair59@gmail.com>
2019-03-18 00:24:56 +0530

    Added a dummy notification functionality

--- app/src/main/java/com/saraswitty/wittyapp/AuthenticatedActivity.kt
+++ app/src/main/java/com/saraswitty/wittyapp/AuthenticatedActivity.kt
@@ -2,7 +2,6 @@ package com.saraswitty.wittyapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.facebook.AccessToken
@@ -10,12 +9,34 @@ import com.facebook.GraphRequest
 import com.facebook.HttpMethod
 import com.facebook.login.LoginManager
 import mu.KotlinLogging
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.support.v4.app.NotificationCompat
+import android.view.View
+import android.annotation.SuppressLint
 
+// TODO Understand why we need the below line
+@SuppressLint("ByteOrderMark")
 class AuthenticatedActivity : AppCompatActivity() {
     private val logger = KotlinLogging.logger {}
+    private var notificationManager: NotificationManager? = null
+
+    // API 26 and above requires a notification channel to send notification
+    private fun createNotificationChannel(id: String, name: String,
+                                  description: String) {
+        if (Build.VERSION.SDK_INT < 26) return
+
+        val importance = NotificationManager.IMPORTANCE_LOW
+        val channel = NotificationChannel(id, name, importance)
+
+        channel.description = description
+        notificationManager?.createNotificationChannel(channel)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.authenticated_activity)
 
         // Get the current fb access token
@@ -49,5 +70,32 @@ class AuthenticatedActivity : AppCompatActivity() {
                 }).executeAsync()
             }
         })
+
+        notificationManager =
+                getSystemService(
+                        Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChannel(
+                "com.saraswitty.wittymail.allNotifications",
+                "Notifications",
+                "Notifications")
+    }
+
+    // Button onClick function handler
+    fun sendNotification(view: View) {
+        val channelID = "com.saraswitty.wittymail.allNotifications"
+        val notificationID = 101
+
+        // Use NotificationCompat instead of Notification for backward compatibility
+        val notification = NotificationCompat.Builder(this@AuthenticatedActivity,
+                channelID)
+                .setContentTitle("Example Notification")
+                .setContentText("This is an  example notification.")
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setChannelId(channelID)
+                .build()
+
+        // Send the notification
+        notificationManager?.notify(notificationID, notification)
     }
 }
---
+++ app/src/main/java/com/saraswitty/wittyapp/NewUpcomingEvent.kt
@@ -0,0 +1,12 @@
+package com.saraswitty.wittyapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewUpcomingEvent : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_upcoming_event)
+    }
+}
